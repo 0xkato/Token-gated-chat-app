42fd97b7504a6750bc601f692b7fdd3245cff4b9
0xKato <emtolihi@gmail.com>
2022-09-19 13:51:29 +0200

    update to home.jsx

--- webpages/src/home.jsx
+++ webpages/src/home.jsx
@@ -1,10 +1,24 @@
+import lock_img from "./imgs/lock.png";
+import chat_img from "./imgs/chat.png";
+
 function Home() {
     
     return (
       <div className="Home">
-          <h1>Home Page</h1>
-          <p>This will be the landing page </p>
-      </div>
+    <h1>Home</h1>
+    <p>Decent chat is a chatting platform, made with the intent of keeping the people of crypto Safe and sound,
+       While comunicating both in terms of there assets but also there data.
+    </p>
+
+
+    <img src={lock_img} className="lock-img" alt="Lock-Icon"/>
+    <p>no trancactions</p>
+
+        <img src={chat_img} className="chat-img" alt="Chat-Icon"/> 
+        <p>Chat with your friends or something</p>
+        </div>
+
+      
     );
   };
   
